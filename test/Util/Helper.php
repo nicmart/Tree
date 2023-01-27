35ad0b3b8b5a5e77ace46d4f416c229f04462b88
Andreas Möller <am@localheinz.com>
2023-01-27 17:20:38 +0100

    Enhancement: Pull in helper from ergebnis/php-package-template

---
+++ test/Util/Helper.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2013-2023 Nicolò Martini
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/nicmart/Tree
+ */
+
+namespace Tree\Test\Util;
+
+use Faker\Factory;
+use Faker\Generator;
+
+trait Helper
+{
+    final protected static function faker(string $locale = 'en_US'): Generator
+    {
+        /**
+         * @var array<string, Generator>
+         */
+        static $fakers = [];
+
+        if (!\array_key_exists($locale, $fakers)) {
+            $faker = Factory::create($locale);
+
+            $faker->seed(9001);
+
+            $fakers[$locale] = $faker;
+        }
+
+        return $fakers[$locale];
+    }
+}
