6c1d4cfc7a4d42dcaf4a1b010def640e17218938
Andreas Möller <am@localheinz.com>
2025-04-15 15:50:44 +0200

    Fix: Run 'make coding-standards'

--- test/Util/Helper.php
+++ test/Util/Helper.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Copyright (c) 2013-2024 Nicolò Martini
+ * Copyright (c) 2013-2025 Nicolò Martini
  *
  * For the full copyright and license information, please view
  * the LICENSE.md file that was distributed with this source code.
